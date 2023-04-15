216fd2577bd93e8cff6aa7ab9ab907c3e24900ce
Kristof Keseru <Kristof_Keseru@epam.com>
2023-04-15 15:31:44 +0200

    client: created navbar component and tested it in the App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import { Button } from './components/common/Button/button.component';
-import './App.scss';
 import { NavbarComponent } from './components/common/Navbar/navbar.component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavbarComponent />
+        <NavbarComponent navBarItems={[{title:'Home', href:'/', icon:<FontAwesomeIcon icon={faHouse} />}]}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- client/src/components/common/Navbar/navbar.component.tsx
+++ client/src/components/common/Navbar/navbar.component.tsx
@@ -1,17 +1,24 @@
 import { FunctionComponent } from 'react';
+import './navbar.component.scss';
+import { NavbarItem } from './models/navbar-item.model';
 
 export interface NavbarProps {
-    navBarItems?: string[];
+  navBarItems: NavbarItem[];
 }
 
 export const NavbarComponent: FunctionComponent<NavbarProps> = ({
-    navBarItems
+  navBarItems
 }) => {
-    return (
-        <nav className="navbar">
-          <ul className="navbar__list">
-            <li className="navbar__item"> <a href="/">Home</a> </li>
-          </ul>
-        </nav>
-      );
+  return (
+    <nav>
+      <ul>
+        {navBarItems.map(({href, icon, title}, index) => (
+          <li key={index}>
+            {icon}
+            <a href={href}>{title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
